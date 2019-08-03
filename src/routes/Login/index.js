09bcf152dc93c49ca8f907b60d6fd881c1527af7
zhaosi <13162960536@163.com>
2019-08-03 11:48:19 +0800

    修复了bug

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,12 +1,2 @@
 import { lazy } from 'react'
-// import mergeRoute from 'components/mergeRoutes'
-// const Login = React.lazy(() => import('./components/homePage'))
-// const aaa = 888
-
-// export default mergeRoute([
-//   {
-//     path: '/login',
-//     tmpl: Login
-//   }
-// ])
-export default lazy(() => import('./components/homepage'))
+export default lazy(() => import('./components/homePage'))
